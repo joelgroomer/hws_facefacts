82c1c26c8394b40b62688968f544c335d3d043d2
joelgroomer <joelgroomer@users.noreply.github.com>
2024-01-19 20:12:56 -0800

    Initial Commit

---
+++ FaceFacts/FaceFactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FaceFactsApp.swift
+//  FaceFacts
+//
+//  Created by Joel Groomer on 1/19/24.
+//
+
+import SwiftUI
+
+@main
+struct FaceFactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
